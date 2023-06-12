fa9bd890a1557d5649b9315f965840b04da26d31
suresh <suresh@softsuave.com>
2023-06-12 16:02:41 +0530

    Build-

--- dist/components/Custom/Button/index.d.ts
+++ dist/components/Custom/Button/index.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
-import { CustomButtonPropsType } from '../../types/utils';
+import { CustomButtonPropsType } from '../../../types/utils';
 export declare function ActionButton({ clickHandler, text }: CustomButtonPropsType): React.JSX.Element;
 export declare function TextButton({ clickHandler, text }: CustomButtonPropsType): React.JSX.Element;
--- dist/components/Custom/Input/Input.d.ts
+++ dist/components/Custom/Input/Input.d.ts
@@ -1,4 +1,4 @@
-import { CustomInputPropsType } from '../../types/utils';
+import { CustomInputPropsType } from '../../../types/utils';
 export declare const CustomInput: import("@emotion/styled").StyledComponent<CustomInputPropsType & {
     theme?: import("@emotion/react").Theme | undefined;
 }, {}, {}>;
